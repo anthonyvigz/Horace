b7a540b753f10240c33b4b8e680965ca50648288
anthonyvigz <anthonyvigz@gmail.com>
2021-02-28 20:12:15 -0500

    working base

--- horace/src/Components/Horace.jsx
+++ horace/src/Components/Horace.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../Styling/horace.scss"
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Horace() {
-  return <h1>HORACE PAGE</h1>;
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return <div className="mainHorace">
+    <div className="horaceBio">
+      <img       data-aos="fade-in"
+      data-aos-duration="700"
+      data-aos-easing="ease-in-out" src="https://i.imgur.com/5o4wMMG.jpg" width="600" alt="horaceAndWillie" />
+      <div className="textBio">
+        <h4       data-aos="fade-in"
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out">
+        Horace Stoneham is the single constant executive in the
+        continuum of the expansion 
+        of baseball's demographic and geographic diversity.
+        </h4>
+        <h4       data-aos="fade-in"
+      data-aos-duration="1550"
+      data-aos-easing="ease-in-out">
+        He is a baseball man,  schooled by the immortal hall of famers 
+        of baseball...to bring baseball culturally forward, creating 
+        more legendary moments, more hall of fame players, that define
+         our national pastime today as it did 100 years ago than any 
+         other beaseball executive.
+        </h4>
+        <h4       data-aos="fade-in"
+      data-aos-duration="1900"
+      data-aos-easing="ease-in-out">
+        Horace's relevance not only for baseball, but for his 
+        economic impact along with being a driving force at the
+         forefront of integrating baseball across cultures, from 
+         the negro leagues, Latin America and Japan, he  belongs in 
+         Cooperstown, as the Giant among leagends that have emanated
+          from his vision for the future of baseball. We live that
+           vision today in Baseball. 
+        </h4>
+        <h4       data-aos="fade-in"
+      data-aos-duration="2500"
+      data-aos-easing="ease-in-out">
+        Horace Stoneham, a baseball man, ahead of his time.
+        </h4>
+    </div>
+  </div>
+  </div>
 }
--- horace/src/Components/Navbar.jsx
+++ horace/src/Components/Navbar.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "../Styling/navbar.scss";
+import useWindowDimensions from "./WindowDimensions";
 
 export default function Navbar() {
+  const { height, width } = useWindowDimensions();
+
   return (
     <div className="nav">
       <NavLink to="/" className="logo">
         <img src="https://i.imgur.com/tJv2gbj.png" width="200px" alt="logo" />
       </NavLink>
       <nav className="links">
+      {width > 950 ? (
+            <nav className="links">
         <NavLink to="/horace">HORACE</NavLink>
         <NavLink to="#docs">DOCUMENTS & CONTENT</NavLink>
         <NavLink to="#legends">LEGENDS</NavLink>
+        </ nav>
+      ) : null}
       </nav>
     </div>
   );
