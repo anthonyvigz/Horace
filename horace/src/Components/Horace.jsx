5e6ff3aed237db49b084470fdd2d3d0d076c05a1
anthonyvigz <anthonyvigz@gmail.com>
2021-04-06 18:30:56 -0400

    quick fix

--- horace/src/Components/Horace.jsx
+++ horace/src/Components/Horace.jsx
@@ -14,10 +14,10 @@ export default function Horace() {
       Here is his story. <i class="fas fa-file-alt"></i>
     </a>
     <div className="horaceBio">
-      <div className="textBio">
-        <h4       data-aos="fade-in"
+      <div data-aos="fade-in"
       data-aos-duration="2000"
-      data-aos-easing="ease-in-out">
+      data-aos-easing="ease-in-out" className="textBio">
+        <h4>
         <span id="horacename">Horace Stoneham</span> is my grandfather. His story and contribution to the 
         game of baseball has been lost to history. You may not know his name, 
         but you live and benefit from his impact and legacy every day. Of the 
@@ -25,24 +25,18 @@ export default function Horace() {
         Horace Stoneham is the catalyzing owner in the continuum of baseball's 
         demographic and geographic expansion.
         </h4>
-        <h4       data-aos="fade-in"
-      data-aos-duration="3000"
-      data-aos-easing="ease-in-out">
+        <h4>
         He is a baseball man, schooled by the immortal Hall of Famers, 
         yet he brought baseball culturally forward, creating more legendary
         moments, more Hall of Fame players, more places to experience baseball 
         than any other baseball executive.
         </h4>
-        <h4       data-aos="fade-in"
-      data-aos-duration="5000"
-      data-aos-easing="ease-in-out">
+        <h4>
         Please help me get Horace In the Hall 
         for the Class of 2022. I would love to hear from you. You can email me at: <span><a target="_blank"
             rel="noopener noreferrer" href="mailto: info@horaceinthehall.com">info@horaceinthehall.com <i class="fas fa-envelope"></i></a></span>
         </h4>
-        <h4       data-aos="fade-in"
-      data-aos-duration="3000"
-      data-aos-easing="ease-in-out">
+        <h4>
         I am most grateful, <span>Jaime Rupert</span>
         </h4>
     </div>
